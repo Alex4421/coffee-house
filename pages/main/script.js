dfdfdf369c93f3d64daddc353c7201c7f67c2efb
Alex4421 <cherevko.alex96@gmail.com>
2024-10-26 14:50:09 +0300

    feat: add js code burger

--- pages/main/script.js
+++ pages/main/script.js
@@ -3,6 +3,12 @@ const leftButton = document.getElementById('button-left');
 const sliderFrames = document.getElementById('slider-frames');
 const slides = document.querySelectorAll('.slider-frames > div');
 const totalSlides = slides.length;
+const buttonMenu = document.getElementById('burger-btn');
+const burgerMenu = document.getElementById('burger-menu7');
+const body = document.getElementById('body');
+const lineOne = document.getElementById('burger-line-1');
+const lineTwo = document.getElementById('burger-line-2');
+let isToggled = false;
 
 
 let currentIndex = 0;
@@ -25,3 +31,24 @@ leftButton.addEventListener('click', function() {
         currentIndex = totalSlides - 1;}
     updateSlider();
 });
+
+ buttonMenu.addEventListener('click', function(){
+    
+        isToggled = !isToggled;
+        if (isToggled) {
+            burgerMenu.style.left = '0%';
+            burgerMenu.style.transition = '0.5s';
+            body.style.overflow ='hidden';
+            lineOne.style.transform = 'rotate(-45deg)';
+            lineOne.style.top = '49%'
+            lineTwo.style.transform = 'rotate(45deg)';
+            lineTwo.style.top = '51%';
+        } else {
+            burgerMenu.style.left = '100%';
+            body.style.overflow ='auto';
+            lineOne.style.transform = 'rotate(0deg)';
+            lineTwo.style.transform = 'rotate(0deg)';
+            lineOne.style.top = '36%'
+            lineTwo.style.top = '59%';
+        }
+ })
